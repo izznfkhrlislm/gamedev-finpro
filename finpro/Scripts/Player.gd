ec9eb800a40460c0bc77f5a4432e081380e92dd1
Imran Amrulloh <imran140898@gmail.com>
2019-11-27 10:18:34 +0700

    rabbit spawn done, still random based on world not the tile position

--- finpro/Scripts/Player.gd
+++ finpro/Scripts/Player.gd
@@ -4,44 +4,63 @@ extends KinematicBody2D
 # var a = 2
 # var b = "text"
 
-export (int) var speed = 200
+export (int) var speed = 1500
 
-onready var direction = 'up'
-onready var axis = 'y'
+onready var world = get_tree().current_scene
+onready var width = get_viewport().size.x
 
+var direction = 'up'
+var axis = 'y'
+var counter = 0
+var rabbit = preload("res://Scenes/Rabbit.tscn")
 var velocity = Vector2()
 
-func get_input():
-	if Input.is_action_pressed('ui_right'):
-		$Sprite.flip_h = false
-		direction = 'right'
-		axis = 'x'
-	if Input.is_action_pressed('ui_left'):
-		$Sprite.flip_h = true
-		direction = 'left'
-		axis = 'x'
-	if Input.is_action_pressed('ui_up'):
-		direction = 'up'
-		axis = 'y'
-	if Input.is_action_pressed('ui_down'):
-		direction = 'down'
-		axis = 'y'
-	if Input.is_action_pressed('jump'):
-		if axis == 'y':
-			if direction == 'up':
-				velocity.y -= speed
+func _input(event):
+	if event is InputEventKey:
+		if event.is_action_pressed('ui_right'):
+			$Sprite.flip_h = false
+			direction = 'right'
+			axis = 'x'
+		if event.is_action_pressed('ui_left'):
+			$Sprite.flip_h = true
+			direction = 'left'
+			axis = 'x'
+		if event.is_action_pressed('ui_up'):
+			direction = 'up'
+			axis = 'y'
+		if event.is_action_pressed('ui_down'):
+			direction = 'down'
+			axis = 'y'
+		if event.is_action_pressed('jump'):
+			if axis == 'y':
+				if direction == 'up':
+					velocity.y -= speed
+					counter += 1
+				else:
+					velocity.y += speed
 			else:
-				velocity.y += speed
-		else:
-			if direction == 'right':
-				velocity.x += speed
-			else:
-				velocity.x -= speed
+				if direction == 'right':
+					velocity.x += speed
+				else:
+					velocity.x -= speed
+			check_jump_count()
+		
+func check_jump_count():
+	randomize()
+	print(counter)
+	print($Camera2D.get_camera_position())
+	if counter == 20:
+		var height = $Camera2D.get_camera_position().y
+		var rabbit_instance = rabbit.instance()
+		rabbit_instance.set_position(Vector2(rand_range(0, width), height-300))
+		if world != null:
+			world.add_child(rabbit_instance)
+		counter = 0
 
 func _physics_process(delta):
-	get_input()
 	velocity = move_and_slide(velocity)
 	velocity = Vector2.ZERO
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 # func _process(delta):
 # pass
