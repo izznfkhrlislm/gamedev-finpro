7e7058e05d41a912373dba2719ad0473e4e8f463
Imran Amrulloh <imran140898@gmail.com>
2019-12-01 20:56:26 +0700

    change player sprite, add animatedsprite but still have bug animation

--- finpro/Scripts/Player.gd
+++ finpro/Scripts/Player.gd
@@ -4,7 +4,7 @@ extends KinematicBody2D
 # var a = 2
 # var b = "text"
 
-export (int) var speed = 1500
+export (int) var speed = 1600
 export (NodePath) var camera_path
 
 onready var world = get_tree().current_scene
@@ -12,8 +12,8 @@ onready var width = get_viewport().size.x
 onready var height = get_viewport().size.y
 onready var camera = get_node(camera_path)
 
-var direction = 'up'
 var axis = 'y'
+var direction = 'up'
 var counter = 0
 var rabbit = preload("res://Scenes/Rabbit.tscn")
 var velocity = Vector2()
@@ -23,41 +23,44 @@ func _input(event):
 	if event is InputEventKey:
 		if event.is_action_pressed('ui_right'):
 			$Sprite.flip_h = false
+			$Sprite.play("IdleV")
 			direction = 'right'
 			axis = 'x'
 		if event.is_action_pressed('ui_left'):
 			$Sprite.flip_h = true
+			$Sprite.play("IdleV")
 			direction = 'left'
 			axis = 'x'
 		if event.is_action_pressed('ui_up'):
+			$Sprite.play("IdleUP")
 			direction = 'up'
 			axis = 'y'
 		if event.is_action_pressed('ui_down'):
+			$Sprite.play("IdleDOWN")
 			direction = 'down'
 			axis = 'y'
 		if event.is_action_pressed('jump'):
-			if axis == 'y':
-				if direction == 'up':
-					velocity.y -= speed
-					counter += 1
-				else:
-					velocity.y += speed
+			if direction == 'up':
+				velocity.y -= speed
+				counter += 1
+			elif direction == 'down':
+				velocity.y += speed
+			elif direction == 'right':
+				velocity.x += speed
 			else:
-				if direction == 'right':
-					velocity.x += speed
-				else:
-					velocity.x -= speed
+				velocity.x -= speed
+			print(velocity)
 			check_jump_count()
 
 
 func check_jump_count():
-	randomize()
 	if counter == 20:
 		create_rabbit_instance()
 		counter = 0
 
 
 func create_rabbit_instance():
+	randomize()
 	var rabbit_instance = rabbit.instance()
 	rabbit_instance.set_position(Vector2(rand_range(0, width), camera.position.y-300))
 	if world != null:
@@ -66,12 +69,20 @@ func create_rabbit_instance():
 
 func _physics_process(delta):
 	velocity = move_and_slide(velocity)
+	if velocity.x != 0:
+		$Sprite.play("JumpV")
+		
+	if velocity.y != 0:
+		if direction == "up":
+			$Sprite.play("JumpUP")
+		else:
+			$Sprite.play("JumpDOWN")
 	velocity = Vector2.ZERO
 
 
 func exit_screen():
 	if position.y > camera.position.y + height/2:
-		pass #player dead
+		get_tree().change_scene("res://Scenes/GameOver.tscn")
 		
 	if position.x > camera.position.x and axis == 'x':
 		position = Vector2(32, position.y)
