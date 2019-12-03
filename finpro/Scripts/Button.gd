702933205a1ddc6812479941da247eb767a5b77f
Izzan Fakhril Islam <izzan.fakhril@ui.ac.id>
2019-12-04 01:12:27 +0700

    editing world

--- finpro/Scripts/Button.gd
+++ finpro/Scripts/Button.gd
@@ -1,6 +1,6 @@
 extends LinkButton
 
 func _on_Start_pressed():
-	var scene_path = "res://Scenes/World" + str(2) + ".tscn"
+	var scene_path = "res://Scenes/World" + str(randi() % 6 + 1) + ".tscn"
 	print(scene_path)
 	get_tree().change_scene(scene_path)
