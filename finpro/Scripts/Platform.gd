654801f2175882a32b3c0ebf1e06b812b4f80a3b
Imran Amrulloh <imran140898@gmail.com>
2019-12-03 10:47:56 +0700

    generate map endless

---
+++ finpro/Scripts/Platform.gd
@@ -0,0 +1,13 @@
+extends KinematicBody2D
+
+export (int) var speed = -100
+var velocity = Vector2()
+
+func _process(delta):
+	if position.x < -get_viewport().size.x or position.x > get_viewport().size.x:
+		queue_free() 
+
+func _physics_process(delta):
+#	velocity.y += delta * GRAVITY
+	velocity.x = speed
+	velocity = move_and_slide(velocity)
