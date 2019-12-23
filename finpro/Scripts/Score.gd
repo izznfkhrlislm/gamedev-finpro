40b5b8e514776b8a8d2ab9082a0f2b5f3107340d
Firandra Savitri <firandra.savitri@gmail.com>
2019-12-23 12:31:41 +0700

    added score in each world

--- finpro/Scripts/Score.gd
+++ finpro/Scripts/Score.gd
@@ -1,5 +1,4 @@
 extends Label
 
-func _ready():
-	global.score = global.score if global.score % 2 == 0 else global.score-1
-	self.text = "Score:\n" + str(global.score)
+func _process(delta):
+	self.text = "Score: " + str(global.score)
